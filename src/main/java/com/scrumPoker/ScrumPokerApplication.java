eaad0e18cb9d94e6add4491a27637616a9fffece
mkorkuts <korkuzmax@gmail.com>
2016-03-02 12:28:15 +0200

    Initial commit

---
+++ src/main/java/com/scrumPoker/ScrumPokerApplication.java
@@ -0,0 +1,12 @@
+package com.scrumPoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPokerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/scrumPoker/ScrumPokerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.scrumPoker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ScrumPokerApplication.class)
+@WebAppConfiguration
+public class ScrumPokerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
