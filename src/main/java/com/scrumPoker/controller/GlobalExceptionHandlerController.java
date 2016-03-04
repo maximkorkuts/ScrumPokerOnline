e93abcdee0712204a194c199f9c52300621fd0f8
mkorkuts <korkuzmax@gmail.com>
2016-03-04 11:44:00 +0200

    Add materialize framework and create first page.

---
+++ src/main/java/com/scrumPoker/controller/GlobalExceptionHandlerController.java
@@ -0,0 +1,10 @@
+package com.scrumPoker.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author Maxim Korkuts
+ */
+@ControllerAdvice
+public class GlobalExceptionHandlerController {
+}
